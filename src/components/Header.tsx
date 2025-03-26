af4455609e313a9e733f3e24011c0688298a9660
Alan <Alan.Shi@duomai.com>
2025-03-26 15:33:56 +0800

    解决部署问题

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,7 +12,7 @@ export default async function Header() {
     <header className="header absolute left-0 right-0 top-0 z-50 -h-32/48 -px-4/6 -py-4/6 hd:h-32">
       <div className="mx-auto grid w-full max-w-6xl grid-cols-[auto,auto] items-center gap-6 md:grid-cols-[1fr,auto,1fr]">
         <Link href="/" className="justify-self-start">
-          <Logo className="text-brand-purple ~h-12/20" />
+          <Logo />
         </Link>
         <nav
           aria-label="Main"
